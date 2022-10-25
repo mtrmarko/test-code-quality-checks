c9f2f0ab8437dc1199ec82b30eebc595df52c4d0
Marko Mitrovic <marko@nuclearn.ai>
2022-10-25 15:58:17 -0700

    take 19

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import requests
 
 root = os.getcwd()
 mydate = datetime.datetime.now()
-print(f'In folder: {root} on this date and time: {mydate} hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+print(f'In folder: {root} on this date and time: {mydate} hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhbhhhhh')
 response = requests.get("https://google.com/")
 print(response.content)
