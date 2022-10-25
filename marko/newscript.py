e20fc6cbda38c1b1cc6aa612d1d8426ba03aa2cc
Marko Mitrovic <marko@nuclearn.ai>
2022-10-25 12:59:06 -0700

    take 8

--- marko/newscript.py
+++ marko/newscript.py
@@ -1,9 +1,9 @@
-import requests
 import datetime
 import os
+import requests
 
 root = os.getcwd()
 mydate = datetime.datetime.now()
-print(f'In subfolder: {root} on this date and time: {mydate}')
-response = requests.get('https://yahoo.com/')
+print(f"In subfolder: {root} on this date and time: {mydate}")
+response = requests.get("https://yahoo.com/")
 print(response.content)
