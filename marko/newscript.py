d434d9a928545a6213f25e50236098d01a28ab3f
Marko Mitrovic <marko@nuclearn.ai>
2022-10-26 10:12:40 -0700

    take 32

--- marko/newscript.py
+++ marko/newscript.py
@@ -2,9 +2,10 @@ import datetime
 import os
 
 import requests
+import sqlite3
 
 root = os.getcwd()
 mydate = datetime.datetime.now()
-print(f"In subfolder: {root} on this date and time: {mydate}")
+print(f"In subfolder: {root} on this date and time: {mydate} blllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll")
 response = requests.get("https://yahoo.com/")
 print(response.content)
--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-import sqlite3
 import datetime
 import os
 import requests
 
 root = os.getcwd()
 mydate = datetime.datetime.now()
-print(f'In folder: {root} on this date and time: {mydate} hhhhhhhhhhhhhhhhhhhgjhghhhhhhhhhhhhhhbhhhmhjhkj')
+print(f'In folder: {root} on this date and time: {mydate} hhhhhhhhhhhhhhhhgjhghhhhhhhhhhhhhhbhhhmhjhkj')
 response = requests.get("https://google.com/")
 print(response.content)
