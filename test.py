c6cd9882264dc4548dd455bfaad801a3bf422097
Marko Mitrovic <marko@nuclearn.ai>
2022-10-25 17:23:10 -0700

    take 21

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-import sqlite3
 import datetime
 import os
 import requests
 
 root = os.getcwd()
 mydate = datetime.datetime.now()
-print(f'In folder: {root} on this date and time: {mydate} hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhbhhhmhh')
+print(f'In folder: {root} on this date and time: {mydate} hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhbhhhmhjhkjhh')
 response = requests.get("https://google.com/")
 print(response.content)
